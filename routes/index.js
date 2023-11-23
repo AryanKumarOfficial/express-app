5619bfcfe0fe39bcaa4b36e5392c191298d6f309
Aryan Kumar <aryanak9163@gmail.com>
2023-11-23 18:47:31 +0530

    completed register GUI

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'iNOteBook-Home' });
 });
 
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'iNOteBook-About' });
+}
+);
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: 'iNOteBook-Contact' });
+}
+);
+
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'iNOteBook-Login' });
+}
+);
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'iNOteBook-Register' });
+}
+);
+
+router.get('/forgot', function (req, res, next) {
+  res.render('forgot', { title: 'iNOteBook-Forgot' });
+}
+);
+
+
+
+
 module.exports = router;
